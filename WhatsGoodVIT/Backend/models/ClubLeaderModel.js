47fda25cdd69de5d16ab47a80ffe73affd043dfb
Dhruv Samuel <dhruvsamuel05@gmail.com>
2025-02-28 18:03:17 +0530

    clubleader has events

--- WhatsGoodVIT/Backend/models/ClubLeaderModel.js
+++ WhatsGoodVIT/Backend/models/ClubLeaderModel.js
@@ -5,6 +5,7 @@ const ClubLeaderSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   clubName: { type: String, required: true },
+  events: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Event' }]
 }, {
   timestamps: true
 });
